dd809e76920955f4439e3c51e35158ad5e858fd3
onicaps <97927985+onicaps@users.noreply.github.com>
2025-07-30 01:48:09 -0300

    asd

--- netlify/functions/obtener-texto.js
+++ netlify/functions/obtener-texto.js
@@ -1,21 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+const fetch = require('node-fetch');
 
 exports.handler = async () => {
-  const ruta = path.resolve(__dirname, 'texto.json');
-
-  let texto = '';
-  if (fs.existsSync(ruta)) {
-    const data = fs.readFileSync(ruta);
-    texto = JSON.parse(data).texto;
-  }
+  const res = await fetch('https://asd22.netlify.app/.netlify/functions/guardar-texto');
+  const data = await res.json();
 
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // 🔥 Esto habilita CORS
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ texto }),
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({ texto: data.texto || '' }),
   };
 };
